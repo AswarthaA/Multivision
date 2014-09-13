8b0ae09112153dfa2cd55e6d8d3ac48113553f27
AswarthaA <raju.asme@gmail.com>
2014-09-13 15:02:34 +0530

    Basic structure is complete

--- server.js
+++ server.js
@@ -33,14 +33,20 @@ db.once('open', function callback() {
 });
 
 var messageSchema = mongoose.Schema({ message: String });
-var Message = mongoose.model('');
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function (err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
 
 app.get('/partials/:partialPath', function (req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
-app.get('*', function(req,res){
-res.render('index');
+app.get('*', function (req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 })
 console.log('started');
 var port = "3030";
